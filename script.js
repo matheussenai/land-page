122072fbe3a10e170809dd9532441a77623f40eb
matheussenai <matheusmarqueselite@gmail.com>
2022-06-29 20:37:35 -0300

    versao inicial

---
+++ script.js
@@ -0,0 +1,45 @@
+const nome              = "Matheus"
+const sobreNome         = "Marques"
+const data_nascimento   = "19/03/1996"
+
+let altura              = 1.75
+let numero_sapato       = 44
+let idade               = 26
+
+let celular             = "(18) 98154-8752"
+let cor_favorita        = "preto"
+
+let qtde_moto           = null
+let qtde_carro          = null
+
+//         <- o valor
+qtde_carro = 1
+
+altura = 1.76
+
+celular = "(18) 99148-2663"
+
+// comparar/validar/verificar Igualdade
+
+nome.trim().toUpperCase() == "MATHEUS" // true
+
+idade == 26 // true
+
+idade = 25
+
+idade == 25 // true
+
+(true == false) == (true == false)
+
+// Igualdade estrita
+
+nome === "matheus" // true
+
+nome === ["matheus"] // false
+
+idade === 26 // true
+
+idade === "26" // false
+
+// 
+
